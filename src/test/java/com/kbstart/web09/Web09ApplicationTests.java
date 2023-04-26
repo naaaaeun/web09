e7980578f57e122b98c2b2f33a64061041a9e2f3
naaaaeun <naeunlee1223@gmail.com>
2023-04-26 13:13:36 +0900

    0426

---
+++ src/test/java/com/kbstart/web09/Web09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstart.web09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
